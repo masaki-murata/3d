01b72084dde67b39abaaabf83a51e2942d81eba3
DESKTOP-Q4BKBCT\murata <m.murata1982@gmail.com>
2018-06-04 20:46:09 +0900

    no message

--- test_size.py
+++ test_size.py
@@ -27,9 +27,10 @@ def autoencoder(input_shape=(512,256,256,1)):
     
     return model
 
-def embed_mnist(mnist_image,
+def embed_mnist(mnist_image=np.array([]),
                 data_shape=(512,256,256),
-                slices=28,):
+                slices=28,
+                ):
     z,y,x = data_shape[0]//2,data_shape[1]//2,data_shape[2]//2
     embeded = np.zeros(data_shape)
     for z_pos in range(slices):
@@ -53,14 +54,64 @@ def train(data_shape=(512,256,256)):
     print(np.sum(x_train))
     print(x_train.shape)
 
+# validation data を作成する関数
+def make_validation_data(data_shape=(512,256,256),
+                         slices=28,
+                         val_size=100,
+                         mnist_x_test, 
+                         mnist_y_test,
+                         ):
+    val_data = np.zeros((val_size,)+data_shape)
+    for mnist_id in range(val_size):
+        val_data[mnist_id] = embed_mnist(mnist_image=mnist_x_test[mnist_id],
+                data_shape=data_shape,
+                slices=slices,
+                )
+            
+    return val_data, val_label
 
+# オートエンコーダをトレーニングする関数
 def train_autoencoder(batch_size=32,
                       nb_gpus=4,
                       data_shape=(512,256,256)):
     input_shape = data_shape + (1,)
     model_single_gpu = autoencoder(input_shape=input_shape)
     model = multi_gpu_model(model_single_gpu, gpus=nb_gpus)
+
+    (mnist_x_train, mnist_y_train), (mnist_x_test, mnist_y_test) = mnist.load_data()
+    
+    # validation data を作る
+    val_data,val_label = make_validation_data(data_shape=(512,256,256),
+                         slices=28,
+                         mnist_x_test, 
+                         mnist_y_test
+                         )
+    
+    # training data の generator をセット
+    train_gen = batch_iter(images=train_images,
+                           segmentation_gts=train_gts, 
+                           crop_shape=crop_shape,
+                           steps_per_epoch=steps_per_epoch,
+                           batch_size=batch_size,
+                           )
+    
+    # for ループで 1 epoch ずつ学習
+    for epoch in range(1,epochs+1):
+        model.fit_generator(train_gen,
+                            steps_per_epoch=steps_per_epoch,
+                            epochs=1,
+                            validation_data=(val_data,val_label)
+                            )
+        print('Epoch %s/%s done' % (epoch, epochs))
+        print("")
         
+        if epoch>0 and epoch % 1==0:
+            print(epoch)
+            model_single_gpu.save(path_to_save_model % (epoch))
+            model_single_gpu.save_weights(path_to_save_weights % (epoch))
+    
+    
+    
 
 def main():
     print("aho")
