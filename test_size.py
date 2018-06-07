9124ebc1f3b9b73a50f24971c9ccf1946bf70437
DESKTOP-Q4BKBCT\murata <m.murata1982@gmail.com>
2018-06-07 17:38:39 +0900

    no message

--- test_size.py
+++ test_size.py
@@ -16,11 +16,11 @@ from keras.utils.training_utils import multi_gpu_model
 # define autoencoder
 def autoencoder(input_shape=(512,256,256,1)):
     input_img = Input(shape=input_shape)
-    x = Conv3D(filters=8, kernel_size=(3,3,3), strides=(3,3,3), padding="same", activation="relu")(input_img)
+    x = Conv3D(filters=8, kernel_size=(2,2,2), strides=(2,2,2), padding="same", activation="relu")(input_img)
     
     # decoding
-    x = UpSampling3D(size=(3,3,3))(x)
-    decoded = Conv3D(filters=1, kernel_size=(3,3,3), padding="same", activation="relu")(x)
+    x = UpSampling3D(size=(2,2,2))(x)
+    decoded = Conv3D(filters=1, kernel_size=(2,2,2), padding="same", activation="relu")(x)
     
     model = Model(input_img, decoded)
     model.compile(optimizer='adam', loss='binary_crossentropy')
@@ -57,19 +57,22 @@ def embed_mnist(mnist_image=np.array([]),
 #    print(x_train.shape)
 
 # validation data を作成する関数
-def make_validation_data(data_shape=(512,256,256),
-                         slices=28,
-                         val_size=100,
-                         mnist_x_test, 
+def make_validation_data(mnist_x_test,
                          mnist_y_test,
+                         data_shape=(512,256,256),
+                         slices=28,
+                         val_size=100,                          
                          ):
     val_data = np.zeros((val_size,)+data_shape)
+    val_label = np.zeros((val_size,))
     for mnist_id in range(val_size):
         val_data[mnist_id] = embed_mnist(mnist_image=mnist_x_test[mnist_id],
                 data_shape=data_shape,
                 slices=slices,
                 )
-        val_data.reshape(val_data.shape+(1,))
+        val_label[mnist_id] = mnist_y_test[mnist_id]
+    val_data = val_data.reshape(val_data.shape+(1,))
+    val_label = val_label.reshape(val_label.shape+(1,))
             
     return val_data, val_label
 
@@ -84,15 +87,17 @@ def batch_iter(mnist_x_train=np.array([]),
         
     while True:
         for step in range(steps_per_epoch):
-            data = np.zeros( (batch_size,)+data_shape+(1,), dtype=np.uint8 )
-            labels = np.zeros( (batch_size,)+data_shape+(1,), dtype=np.uint8 )
+            data = np.zeros( (batch_size,)+data_shape, dtype=np.uint8 )
+            labels = np.zeros( (batch_size,)+data_shape, dtype=np.uint8 )
             for count in range(batch_size):
-                data[count] = embed_mnist(mnist_image=mnist_x_test[count],
+                data[count] = embed_mnist(mnist_image=mnist_x_train[count],
                                           data_shape=data_shape,
                                           slices=slices,
                                           )
                 labels[count] = mnist_y_train[count]
-            data.reshape(data.shape+(1,))
+            data = data.reshape(data.shape+(1,))
+            labels = labels.reshape(labels.shape+(1,))
+#            print("data.shape = ", data.shape)
             yield data, labels
     
     
@@ -100,26 +105,41 @@ def batch_iter(mnist_x_train=np.array([]),
 # オートエンコーダをトレーニングする関数
 def train_autoencoder(batch_size=32,
                       nb_gpus=4,
-                      data_shape=(512,256,256)):
+                      data_shape=(512,256,256),
+                      slices=28,
+                      steps_per_epoch=32,
+                      epochs=64,
+                      val_size=100,
+                      ):
     # setting model
     input_shape = data_shape + (1,)
     model_single_gpu = autoencoder(input_shape=input_shape)
-    model = multi_gpu_model(model_single_gpu, gpus=nb_gpus)
+    if nb_gpus>1:
+        model = multi_gpu_model(model_single_gpu, gpus=nb_gpus)
+    elif nb_gpus==1:
+        model = model_single_gpu
 
     # load mnist dataset
-    (mnist_x_train, mnist_y_train), (mnist_x_test, mnist_y_test) = mnist.load_data()
+    mnist_x_train = np.load("./mnist_x_train.npy")
+    mnist_y_train = np.load("./mnist_y_train.npy")
+    mnist_x_test = np.load("./mnist_x_test.npy")
+    mnist_y_test = np.load("./mnist_y_test.npy")
+#    (mnist_x_train, mnist_y_train), (mnist_x_test, mnist_y_test) = mnist.load_data()
     
     # set validation data
-    val_data,val_label = make_validation_data(data_shape=(512,256,256),
-                         slices=28,
-                         mnist_x_test, 
-                         mnist_y_test
-                         )
+    val_data, val_label = make_validation_data(mnist_x_test=mnist_x_test,
+                                              mnist_y_test=mnist_y_test,
+                                              data_shape=data_shape,
+                                              slices=slices,
+                                              val_size=val_size,
+                                              )
+    print("val_data.shape = ", val_data.shape)
     
     # set generator for training data
-    train_gen = batch_iter(images=train_images,
-                           segmentation_gts=train_gts, 
-                           crop_shape=crop_shape,
+    train_gen = batch_iter(mnist_x_train=mnist_x_train,
+                           mnist_y_train=mnist_y_train, 
+                           slices=slices,
+                           data_shape=data_shape,
                            steps_per_epoch=steps_per_epoch,
                            batch_size=batch_size,
                            )
@@ -129,7 +149,7 @@ def train_autoencoder(batch_size=32,
         model.fit_generator(train_gen,
                             steps_per_epoch=steps_per_epoch,
                             epochs=1,
-                            validation_data=(val_data,val_label)
+#                            validation_data=(val_data,val_label)
                             )
         print('Epoch %s/%s done' % (epoch, epochs))
         print("")
@@ -144,8 +164,15 @@ def train_autoencoder(batch_size=32,
     
 
 def main():
-    print("aho")
-    train()
+#    print("aho")
+    train_autoencoder(batch_size=1,
+                      nb_gpus=1,
+                      data_shape=(512,256,256),
+                      slices=28,
+                      steps_per_epoch=32,
+                      epochs=64,
+                      val_size=100,
+                      )
     
 if __name__ == '__main__':
     main()
